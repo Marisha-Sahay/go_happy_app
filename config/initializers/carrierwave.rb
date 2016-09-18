b6fa3c9ec7d08ac136dc4e8a235799f24dede65b
Marisha Sahay <marishasahay@gmail.com>
2016-09-18 15:14:54 -0700

    Add amazon S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      ENV['S3_KEY'],
-    aws_secret_access_key:  ENV['S3_SECRET'],
+    aws_access_key_id:      ENV['access_key_id'],
+    aws_secret_access_key:  ENV['secret_access_key'],
   }
 
   # For testing, upload files to local `tmp` folder.
