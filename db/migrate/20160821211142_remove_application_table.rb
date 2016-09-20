7ac7d53a2c67f65af708e509a52935cf1ef52ac7
Marisha Sahay <marishasahay@gmail.com>
2016-09-19 20:56:42 -0700

    heroku errors persist

--- db/migrate/20160821211142_remove_application_table.rb
+++ db/migrate/20160821211142_remove_application_table.rb
@@ -1,6 +1,6 @@
 class RemoveApplicationTable < ActiveRecord::Migration[5.0]
   def change
-    drop_table :applications
+    # drop_table :applications
     add_column :users, :phone_no, :integer
     rename_column :users, :name, :first_name
     add_column :users, :last_name, :string
