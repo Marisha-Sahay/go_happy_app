b01620ab77cb6bd8ee25a147b2d9fa8941f58a40
Marisha Sahay <marishasahay@gmail.com>
2016-09-20 20:59:30 -0700

    Add image upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,17 +6,8 @@ CarrierWave.configure do |config|
     provider:               'AWS',
     aws_access_key_id:      ENV['access_key_id'],
     aws_secret_access_key:  ENV['secret_access_key'],
+    region: 'us-west-2',
+    path_style: true
   }
-
-  # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage           = :file
-    config.enable_processing = false
-    config.root              = "#{Rails.root}/tmp"
-  else
-    config.storage = :file
-  end
-
-  config.cache_dir        = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on Heroku
   config.fog_directory    = ENV['S3_BUCKET_NAME']
 end
